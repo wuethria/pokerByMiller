9644aad7f8ceb6062d84e2c13898d82be7c9021e
Adrian Wüthrich <adrian.wuethrich@bit.admin.ch>
2026-02-27 23:54:36 +0100

    Update trainer UI and strategy PDF to v9

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'poker-preflop-v2';
+const CACHE_NAME = 'poker-preflop-v4';
 const ASSETS = [
   './',
   './index.html',
   './manifest.webmanifest',
   './sw.js',
-  './Poker_Preflop_Strategie_FINAL_v7.pdf',
+  './Poker_Preflop_Strategie_FINAL_v9.pdf',
   './icons/icon-192.png',
   './icons/icon-512.png'
 ];
